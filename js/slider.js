3c412f7e734afff7b80a74c5c27557e518bd037b
wendell00 <wendellborges2003@gmail.com>
2022-08-04 20:11:39 -0300

    Fazendo o Carousel

---
+++ js/slider.js
@@ -0,0 +1,105 @@
+//PORTFOLIO SLIDER
+
+//Declarando variáveis do slider
+var sliderContainer = document.querySelector('.cs-slider-container');
+var sliderList = document.querySelector('.cs-slider-list');
+var sliderItem = document.querySelectorAll('.cs-slider-item');
+const sliderTotalItems = sliderItem.length;
+var sliderListWidth = null;
+var prevItem = document.querySelector('.cs-item-prev');
+var nextItem = document.querySelector('.cs-item-next');
+var sliderPos = 0;
+var currentSlide = document.querySelector('.cs-current-slide');
+var totalSlide = document.querySelector('.cs-total-slide');
+var currentCounter = 1;
+
+
+//Capturando larguras individuais
+var containerWidth = sliderContainer.parentElement.offsetWidth;
+
+//Passando larguras dinâmicas
+sliderContainer.style.width = containerWidth + 'px';
+
+for (var p = 0; p < sliderItem.length; p++) {
+    sliderItem[p].style.width = containerWidth + 'px';
+    var sliderItemWidth = sliderItem[p].offsetWidth;
+
+    sliderListWidth += sliderItemWidth;
+}
+
+sliderList.style.width = sliderListWidth + 'px';
+
+
+//Fazendo Animaçao do Slider onClick
+
+
+//HANDLERS
+
+//Next Slide Animaçao
+var nextSlideAnim = function () {
+    var lastItem = sliderListWidth - containerWidth;
+
+    if ((-1 * (sliderPos) === lastItem)) {
+        return;
+    }
+
+    sliderPos -= containerWidth;
+    anime({
+        targets: sliderList,
+        translateX: sliderPos
+    });
+}
+
+//Prev Slide Animaçao
+var prevSlideAnim = function () {
+    if (sliderPos === 0) {
+        return;
+    }
+
+    sliderPos += containerWidth;
+    anime({
+        targets: sliderList,
+        translateX: sliderPos
+    });
+}
+
+//Counter Formater
+var counterFormatter = function (n) {
+    if (n < 10) {
+        return '0' + n;
+    } else {
+        return n;
+    }
+}
+
+//Counter Add
+var counterAdd = function () {
+    if ((currentCounter >= 0) && (currentCounter < sliderTotalItems)) {
+        currentCounter++;
+        currentSlide.innerHTML = counterFormatter(currentCounter);
+    }
+}
+
+//Counter Remove
+var counterRemove = function () {
+    if ((currentCounter > 1) && (currentCounter <= sliderTotalItems)) {
+        currentCounter--;
+        currentSlide.innerHTML = counterFormatter(currentCounter);
+    }
+}
+
+
+
+//ACTIONS
+totalSlide.innerHTML = counterFormatter(sliderTotalItems);
+
+
+nextItem.addEventListener('click', function () {
+    nextSlideAnim();
+    counterAdd();
+});
+
+prevItem.addEventListener('click', function () {
+    prevSlideAnim();
+    counterRemove();
+});
