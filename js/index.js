2633376086f3110a1d07f72fc8258923bf7af70d
wendell00 <wendellborges2003@gmail.com>
2022-08-16 20:59:51 -0300

    Adicionando Shake no titulo

--- js/index.js
+++ js/index.js
@@ -1,3 +1,9 @@
 // import '../css/style.css';
 
 var rellax =  new Rellax('.rellax');
+
+let shake = document.querySelector(".title")
+
+setInterval(function () {
+    shake.classList.toggle('shake')
+}, 2000);
