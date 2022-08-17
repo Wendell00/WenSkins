9642f24cf4fd6497cb691e2a28621bc3008a1287
wendell00 <wendellborges2003@gmail.com>
2022-08-17 19:24:57 -0300

    Adicionando animações

--- js/index.js
+++ js/index.js
@@ -3,6 +3,11 @@
 var rellax =  new Rellax('.rellax');
 
 let shake = document.querySelector(".title")
+let greyScale = document.querySelector(".img-main")
+
+greyScale.addEventListener('mouseover', function(e){
+    greyScale.classList.add('greyScaleAnime')
+})
 
 setInterval(function () {
     shake.classList.toggle('shake')
