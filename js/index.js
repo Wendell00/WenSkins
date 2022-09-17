f197bdbe8314cdbf9d4c0a71ca6eb34d988c631d
wendell00 <wendellborges2003@gmail.com>
2022-09-17 07:08:28 -0300

    comentando código e ajustando switch color mob

--- js/index.js
+++ js/index.js
@@ -61,6 +61,27 @@ fecharMenuMob.addEventListener('click', function (e){
     menuMob.classList.toggle('dis-block')
 })
 
+// Fechar/Abrir switch color Mobile
+
+// const fecharMenuMob = document.querySelector('.btn-close-mob')
+const abrirSwitchMob = document.querySelector('.btn-config-mob')
+// const headerBgMob = document.querySelector('.header-bg-mob')
+const menuMobSwitch = document.querySelector('.header-switch-mob')
+// const navMob = document.querySelector('.nav-mob')
+
+abrirSwitchMob.addEventListener('click', function(e){
+    menuMobSwitch.classList.toggle('dis-block')
+    navMob.classList.add('cs-fade-from-right')
+})
+
+// headerBgMob.addEventListener('click', function (e){
+//     menuMobSwitch.classList.toggle('dis-block')
+// })
+
+// fecharMenuMob.addEventListener('click', function (e){
+//     menuMobSwitch.classList.toggle('dis-block')
+// })
+
 // Fechar/Abrir o Trocador de Cores
 
 const switchColor = document.querySelector('.switchColor-bars')
@@ -71,13 +92,7 @@ switchColorContainer.addEventListener('click', function (e){
     switchColor.classList.toggle('cs-scale-up')
 })
 
-// const class_obj = document.querySelectorAll(".switchColor-circle")
-// const nav1 = document.querySelector('.nav-1')
-
-// class_obj.addEventListener('click', (event) =>{
-//     console.log(class_obj)
-//     nav1.classList.add(circulo_name)
-// })
+// Declarando várives dos circulos das cores 
 
 const componentColor = document.querySelectorAll('.switchColorID')
 const componentColorText = document.querySelectorAll('.switchColorTextID')
@@ -86,34 +101,66 @@ const circleRGB = document.querySelector('.circle-rgb')
 const circleBlue = document.querySelector('.circle-blue')
 const circleGreen = document.querySelector('.circle-green')
 
-circleRGB.addEventListener('click', () =>{
 
+// função para que não tenha nenhum circulo ativo
+function removeActiveCircle(){
+    for(var count = 0; count < allCircles.length; count++){
+        allCircles[count].classList.remove('activeCircle')
+
+    }    
+}
+
+// função para mudar a cor do circulo para laranja
+function switchColorToOrange(){
+    for(var count = 0; count < componentColor.length; count++){
+        componentColor[count].style.backgroundImage = ''
+        componentColor[count].style.backgroundColor = '#FCA311';
+    }  
+
+    for(var count = 0; count < componentColorText.length; count++){
+        componentColorText[count].style.background = '#FCA311'
+        componentColorText[count].style.webkitTextFillColor = 'transparent'
+        componentColorText[count].style.webkitBackgroundClip = 'text'
+    }  
+}
+
+// Escutando clicks dos circulos
+
+circleRGB.addEventListener('click', () =>{
+    // Mudando as cores de todos os elementos no HTML com a classe 'switchColorID'
     for(var count = 0; count < componentColor.length; count++){
         componentColor[count].style.backgroundImage = 'linear-gradient(45deg, hsl(240deg 100% 20%) 0%, hsl(289deg 100% 21%) 11%, hsl(315deg 100% 27%) 22%, hsl(329deg 100% 36%) 33%, hsl(337deg 100% 43%) 44%, hsl(357deg 91% 59%) 56%, hsl(17deg 100% 59%) 67%, hsl(34deg 100% 53%) 78%, hsl(45deg 100% 50%) 89%, hsl(55deg 100% 50%) 100%)'
     }    
+
+    // Mudando as cores de todos os elementos no HTML com a classe 'switchColorTextID'
     for(var count = 0; count < componentColorText.length; count++){
         componentColorText[count].style.background = 'linear-gradient(45deg, hsl(240deg 100% 20%) 0%, hsl(289deg 100% 21%) 11%, hsl(315deg 100% 27%) 22%, hsl(329deg 100% 36%) 33%, hsl(337deg 100% 43%) 44%, hsl(357deg 91% 59%) 56%, hsl(17deg 100% 59%) 67%, hsl(34deg 100% 53%) 78%, hsl(45deg 100% 50%) 89%, hsl(55deg 100% 50%) 100%)'
         componentColorText[count].style.webkitTextFillColor = 'transparent'
         componentColorText[count].style.webkitBackgroundClip = 'text'
     }  
 
+    // se o circulo for o último a ter sido clicado vai remover o circulo ativo dos outros e se já estiver ativo muda os elementos para laranja
     if(circleRGB.classList.contains('activeCircle')){
         switchColorToOrange()
     } else{
         removeActiveCircle()
     }
 
+    //Ativa a classe activeCircle
     circleRGB.classList.toggle('activeCircle')
+    //Ativa classe bg-color-rgb pois como é background-image precisa ser desabilitado para usar background-color e não sobrepor   
     circleRGB.classList.toggle('bg-color-rgb')
 })
 
 circleBlue.addEventListener('click', () =>{
+    // adiocinando a cor do botão rgb para evitar que ele esteja laranja
     circleRGB.classList.add('bg-color-rgb')
 
     for(var count = 0; count < componentColor.length; count++){
         componentColor[count].style.backgroundImage = ''
         componentColor[count].style.backgroundColor = '#116afc';
     }    
+
     for(var count = 0; count < componentColorText.length; count++){
         componentColorText[count].style.background = '#116afc'
         componentColorText[count].style.webkitTextFillColor = 'transparent'
@@ -152,23 +199,5 @@ circleGreen.addEventListener('click', () =>{
     circleGreen.classList.toggle('activeCircle')
 })
 
-function removeActiveCircle(){
-    for(var count = 0; count < allCircles.length; count++){
-        allCircles[count].classList.remove('activeCircle')
 
-    }    
-}
-
-function switchColorToOrange(){
-    for(var count = 0; count < componentColor.length; count++){
-        componentColor[count].style.backgroundImage = ''
-        componentColor[count].style.backgroundColor = '#FCA311';
-    }  
-
-    for(var count = 0; count < componentColorText.length; count++){
-        componentColorText[count].style.background = '#FCA311'
-        componentColorText[count].style.webkitTextFillColor = 'transparent'
-        componentColorText[count].style.webkitBackgroundClip = 'text'
-    }  
-}
 
